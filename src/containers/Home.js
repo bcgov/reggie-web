1635c295ef985351146659506a0303e12a5145c4
ShellyXueHan <shellshell456@gmail.com>
2019-01-28 17:10:06 -0800

    refactor home page auth checks

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -21,14 +21,23 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
-import { authorize } from '../actionCreators';
 import { css } from 'react-emotion';
 import { BeatLoader } from 'react-spinners';
+import { Redirect, Link } from 'react-router-dom';
+import { authorize } from '../actionCreators';
+import { ROUTES, AUTH_CODE } from '../constants';
 
 // Here provides option to access different services/apps
 class Home extends Component {
   static displayName = '[Component Home]';
+
+  componentDidUpdate = () => {
+    if (this.props.isAuthenticated && this.props.userId) {
+      if (this.props.userInfo.id === null && !this.props.isAuthorizing) {
+        this.props.authorize('rc', this.props.userId);
+      }
+    }
+  };
 
   render() {
     // get email intention and jwt from localStorage:
@@ -36,43 +45,52 @@ class Home extends Component {
     const intention = localStorage.getItem('emailIntention');
 
     // Set the rendering content based on authentication and authorization:
-    let content = <p>Please log in to SSO to proceed</p>;
+    const authenticationContent = this.props.isAuthenticated ? null : (
+      <p>Please log in to SSO to proceed</p>
+    );
+
+    // Error message:
+    const errMsg =
+      this.props.errorMessages.length > 0 ? <p>{this.props.errorMessages[0]}</p> : null;
+
+    // Loader:
     const override = css`
       display: block;
       margin: 0 auto;
       border-color: #003366;
     `;
-    const loader = <BeatLoader css={override} sizeUnit={'px'} size={25} color="#003366" />;
+    const loader = this.props.isAuthorizing ? (
+      <BeatLoader css={override} sizeUnit={'px'} size={25} color="#003366" />
+    ) : null;
 
-    if (this.props.isAuthenticated) {
-      content = <p>this should not show up</p>;
-      if (this.props.errorMessages.length > 0) {
-        content = <p>{this.props.errorMessages[0]}</p>;
-      } else if (this.props.isAuthorizing) {
-        content = loader;
-      } else if (this.props.userInfo.id === null) {
-        this.props.authorize('rc', this.props.userId);
-      } else {
-        // if there exist email payloads in localstorage, go to confirmation page
-        // if user is authorized for Rocket chat, go to invitation page (as there's only one option atm)
-        // else if user does not meet the requirement to join Rocket chat, go to rejection page
-        // else, stay in the home page untill user pick an option
-        let authorizedRedirect = null;
-        if (emailJwt) {
-          if (intention === 'confirm') {
-            authorizedRedirect = <Redirect to="/confirmation" />;
-          }
-          if (intention === 'invite') {
-            authorizedRedirect = <Redirect to="/verify" />;
-          }
-        } else if (this.props.authCode === 2) {
-          authorizedRedirect = <Redirect to="/rocketChat" />;
-        } else if (this.props.authCode === 3) {
-          authorizedRedirect = <Redirect to="/rejection" />;
-        } else if (this.props.authCode === 0) {
-          authorizedRedirect = <Redirect to="/registration" />;
-        } else {
-          authorizedRedirect = (
+    /*
+      if there exist email payloads in localstorage, go to confirmation page
+      if user is authorized for Rocket chat, go to invitation page (as there's only one option atm)
+      else if user does not meet the requirement to join Rocket chat, go to rejection page
+      else, stay in the home page untill user pick an option
+    */
+    // Redirect based on Email:
+    const setEmailRedirect = (emailJwt, intention) => {
+      if (!emailJwt || !intention) return null;
+      if (intention === ROUTES.EMAIL.CONFIRM) {
+        return <Redirect to="/confirmation" />;
+      }
+      if (intention === ROUTES.EMAIL.VERIFY) return <Redirect to="/verify" />;
+      return null;
+    };
+    const emailRedirect = this.props.isAuthenticated ? setEmailRedirect(emailJwt, intention) : null;
+
+    // Redirect based on authorization:
+    const setAuthorizationRedirect = authCode => {
+      switch (authCode) {
+        case AUTH_CODE.AUTHORIZED:
+          return <Redirect to="/rocketChat" />;
+        case AUTH_CODE.REJECTED:
+          return <Redirect to="/rejection" />;
+        case AUTH_CODE.NEW:
+          return <Redirect to="/registration" />;
+        case AUTH_CODE.PENDING:
+          return (
             <div>
               <p>You have a pending registration!</p>
               <Link className="btn btn-primary" to="/registration">
@@ -80,15 +98,24 @@ class Home extends Component {
               </Link>
             </div>
           );
-        }
-        content = authorizedRedirect;
+        default:
+          return null;
       }
-    }
+    };
+
+    const authorizeRedirect =
+      this.props.isAuthenticated && this.props.userInfo.id
+        ? setAuthorizationRedirect(this.props.authCode)
+        : null;
 
     return (
       <div className="authed">
         <h1>Welcome to Reggie web</h1>
-        {content}
+        {authenticationContent}
+        {errMsg}
+        {loader}
+        {emailRedirect}
+        {authorizeRedirect}
       </div>
     );
   }
