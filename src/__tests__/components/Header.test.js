b1c55d652989c7272a0e8edca786e547aa9779ae
ShellyXueHan <shellshell456@gmail.com>
2019-01-31 12:01:29 -0800

    fix header test case

--- src/__tests__/components/Header.test.js
+++ src/__tests__/components/Header.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/UI/Header';
+import { Header } from '../../components/UI/Header';
 
 describe('Header Component', () => {
   it('matches snapshot when authenticated', () => {
