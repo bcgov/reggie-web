3508509d51e133c1d32887084741e8e92aa8f26c
ShellyXueHan <shellshell456@gmail.com>
2019-02-01 11:11:37 -0800

    add pipeline project folder and jenkins

---
+++ .pipeline/lib/build.js
@@ -0,0 +1,26 @@
+'use strict';
+const {OpenShiftClient, OpenShiftClientX} = require('pipeline-cli')
+const path = require('path');
+
+module.exports = (settings)=>{
+  const oc=new OpenShiftClientX({'namespace':'devhub-tools'});
+  var templateFile = path.resolve(__dirname, '../../openshift/bc.yaml')
+  
+  const appName = 'reggie-web'
+
+  var objects = oc.process(oc.toFileUrl(templateFile), {
+    'param':{
+      'NAME':appName,
+      'SUFFIX':`-${oc.options.pr}`,
+      'VERSION':'1.0.0',
+      'SOURCE_REPOSITORY_URL':`${oc.git.uri}`,
+      'SOURCE_REPOSITORY_REF':`${oc.git.branch_ref}`
+    }
+  })
+
+  oc.applyBestPractices(objects)
+  oc.applyRecommendedLabels(objects, appName, 'build', oc.options.pr)
+  oc.fetchSecretsAndConfigMaps(objects)
+  var applyResult = oc.apply(objects)
+  applyResult.narrow('bc').startBuild()
+}
